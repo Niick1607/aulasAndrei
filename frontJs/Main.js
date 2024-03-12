b5588f5da4c7409569793ce56b3a6e577b438b8a
Niick_1607 <nicolast0ld07@gmail.com>
2024-03-12 11:25:42 -0300

    atividade 12/03

---
+++ frontJs/Main.js
@@ -0,0 +1,34 @@
+const body = document.querySelector('body');
+const clickText = document.getElementById('clicks');
+const img = document.querySelector('img');
+const input = document.getElementById('input');
+let i = 0;
+
+document.getElementById('clicksBt').addEventListener("click", function() {
+    i++;
+    clickText.innerText = `Você clicou ${i} vezes!`;
+});
+
+document.addEventListener("keydown", function(event) {
+    clickText.innerText = `Você clicou na tecla ${event.key.toUpperCase()}!`;
+});
+
+document.getElementById('send').addEventListener("click", function() {
+    alert(`Saudações ${input.value}`);
+    body.style.backgroundColor = document.getElementById('color').value;
+    if (input.value.trim() === '') {
+        input.style.backgroundColor = 'red';
+      } else {
+        input.style.backgroundColor = 'green';
+      }
+});
+
+img.addEventListener('mouseover', function() {
+    img.src = 'acesa.jpeg';
+});
+img.addEventListener('click', function() {
+    img.src = 'apagada.avif';
+});
+
+alert("Mensagem direto no html!");
+alert(2+2);
